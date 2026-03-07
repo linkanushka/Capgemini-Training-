f2b49f7607b4185c39c4ac50f61d8e361e0bcc52
Anushka <125487072+linkanushka@users.noreply.github.com>
2026-03-07 14:58:00 +0530

    Create DBConnection.java

---
+++ Assignment/Product  inventory management/DBConnection.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/inventory_db";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
