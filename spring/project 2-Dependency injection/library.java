9fcdf19ed81cc15aba43c0cf484a5d803b7732ab
Anushka <125487072+linkanushka@users.noreply.github.com>
2026-03-12 00:20:21 +0530

    Create library.java

---
+++ spring/project 2-Dependency injection/library.java
@@ -0,0 +1,39 @@
+package DependencyInjection;
+
+import java.util.List;
+
+public class library {
+
+    private int id;
+    private String name;
+    private List<Book> books;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Library [id=" + id + ", name=" + name + ", books=" + books + "]";
+    }
+}
