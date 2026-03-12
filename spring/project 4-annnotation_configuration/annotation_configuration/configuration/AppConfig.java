a5390978ac70f7d5e1479a7d28499501f74facd9
Anushka <125487072+linkanushka@users.noreply.github.com>
2026-03-12 23:17:53 +0530

    Create AppConfig.java

---
+++ spring/project 4-annnotation_configuration/annotation_configuration/configuration/AppConfig.java
@@ -0,0 +1,12 @@
+package annotation_configuration.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+@PropertySource(value="classpath.d1.property")
+@Configuration
+@ComponentScan(basePackages = "annotation_configuration")
+
+
+public class AppConfig {
+}
